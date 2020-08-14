93e1e0b50acce1b5e42f0bbd31e1c9496cdd9785
yoshidan <naohiro.y@gmail.com>
2020-08-14 14:58:48 +0900

    modify

---
+++ precache-manifest.4401c98b6dcff8832360c63fe1d8ecdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67ac834ea5509648c2e8aa83333d892",
+    "url": "/VRPvP/index.html"
+  },
+  {
+    "revision": "761e465276415882c5e0",
+    "url": "/VRPvP/static/js/2.764a6e14.chunk.js"
+  },
+  {
+    "revision": "c57782abec86592392b58c8a3e982b2a",
+    "url": "/VRPvP/static/js/2.764a6e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7879f8751b1c3de254cd",
+    "url": "/VRPvP/static/js/main.8ab36e99.chunk.js"
+  },
+  {
+    "revision": "fa392e306876a11094d1",
+    "url": "/VRPvP/static/js/runtime-main.4f105740.js"
+  }
+]);
